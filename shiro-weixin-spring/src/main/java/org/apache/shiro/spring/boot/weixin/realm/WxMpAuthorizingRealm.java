891db901dd650f4e63ce225f85ec185700bc0da6
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 01:55:18 +0800

    refactor: split shiro weixin into core and spring modules

---
+++ shiro-weixin-spring/src/main/java/org/apache/shiro/spring/boot/weixin/realm/WxMpAuthorizingRealm.java
@@ -0,0 +1,88 @@
+package org.apache.shiro.spring.boot.weixin.realm;
+
+import java.util.Objects;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.biz.realm.AbstractAuthorizingRealm;
+import org.apache.shiro.biz.realm.AuthorizingRealmListener;
+import org.apache.shiro.spring.boot.weixin.authc.WxMpLoginRequest;
+import org.apache.shiro.spring.boot.weixin.token.WxMpAuthenticationToken;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
+
+import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
+import me.chanjar.weixin.common.bean.oauth2.WxOAuth2AccessToken;
+import me.chanjar.weixin.common.error.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpService;
+
+@Slf4j
+public class WxMpAuthorizingRealm extends AbstractAuthorizingRealm {
+
+
+	private final WxMpService wxMpService;
+
+	public WxMpAuthorizingRealm(WxMpService wxMpService) {
+		this.wxMpService = wxMpService;
+	}
+
+	@Override
+	public Class<? extends AuthenticationToken> getAuthenticationTokenClass() {
+		return WxMpAuthenticationToken.class;
+	}
+
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		log.info("Handle authentication token {}.", token);
+		AuthenticationException ex = null;
+		AuthenticationInfo info = null;
+		try {
+			WxMpAuthenticationToken loginToken = (WxMpAuthenticationToken) token;
+			WxMpLoginRequest loginRequest = (WxMpLoginRequest) loginToken.getPrincipal();
+			if (StringUtils.hasText(loginRequest.getCode())) {
+				WxOAuth2AccessToken accessToken = getWxMpService().getOAuth2Service()
+						.getAccessToken(loginRequest.getCode());
+				if (Objects.nonNull(accessToken)) {
+					loginRequest.setAccessToken(accessToken);
+					loginRequest.setOpenid(accessToken.getOpenId());
+					loginRequest.setUnionid(accessToken.getUnionId());
+				}
+			}
+			if (Objects.isNull(loginRequest.getUserInfo()) && Objects.nonNull(loginRequest.getAccessToken())) {
+				WxOAuth2UserInfo userInfo = getWxMpService().getOAuth2Service()
+						.getUserInfo(loginRequest.getAccessToken(), loginRequest.getLang());
+				if (Objects.nonNull(userInfo)) {
+					loginRequest.setUserInfo(userInfo);
+				}
+			}
+			info = getRepository().getAuthenticationInfo(loginToken);
+		} catch (AuthenticationException e) {
+			ex = e;
+		} catch (WxErrorException e) {
+			ex = new AuthenticationException(e);
+		}
+		notifyListeners(token, ex, info);
+		if (Objects.nonNull(ex)) {
+			throw ex;
+		}
+		return info;
+	}
+
+	private void notifyListeners(AuthenticationToken token, AuthenticationException ex, AuthenticationInfo info) {
+		if (Objects.nonNull(getRealmsListeners()) && !getRealmsListeners().isEmpty()) {
+			for (AuthorizingRealmListener realmListener : getRealmsListeners()) {
+				if (Objects.nonNull(ex) || Objects.isNull(info)) {
+					realmListener.onFailure(this, token, ex);
+				} else {
+					realmListener.onSuccess(this, info);
+				}
+			}
+		}
+	}
+
+	public WxMpService getWxMpService() {
+		return wxMpService;
+	}
+
+}
